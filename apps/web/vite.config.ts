7cd904dfdd852b8dc6f6218bc7ce45dbcf5d49b6
Rubén <ruben@nexolabs.xyz>
2024-10-16 16:05:12 +0200

    fix: Issue in turborepo dev mode fixed

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -24,13 +24,26 @@ export default defineConfig({
 				target: `http://localhost:3000`,
 				changeOrigin: true,
 				secure: false,
-				ws: true
+				ws: true,
+				xfwd: true,
+				configure: (proxy, _options) => {
+					proxy.on('proxyReq', (proxyReq, req, _res) => {
+					  proxyReq.setHeader('host', 'localhost:3000');
+					  proxyReq.setHeader('x-forwarded-host', 'localhost:3000');
+					  proxyReq.setHeader('origin', 'http://localhost:3000');
+				})}
 			},
 			'/admin': {
 				target: `http://localhost:3000`,
 				changeOrigin: true,
 				secure: false,
-				ws: true
+				ws: true,
+				configure: (proxy, _options) => {
+					proxy.on('proxyReq', (proxyReq, req, _res) => {
+					  proxyReq.setHeader('host', 'localhost:3000');
+					  proxyReq.setHeader('x-forwarded-host', 'localhost:3000');
+					  proxyReq.setHeader('origin', 'http://localhost:3000');
+				})}
 			},
 			'/_next': {
 				target: `http://localhost:3000`,
