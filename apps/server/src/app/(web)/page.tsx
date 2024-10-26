6538f427c5cbb9dd7035b147ee335964f0ded9f0
Rubén <ruben@nexolabs.xyz>
2024-10-26 22:50:51 +0200

    test

--- apps/server/src/app/(web)/page.tsx
+++ apps/server/src/app/(web)/page.tsx
@@ -1,13 +1,8 @@
-import AuthOverview from "./_components/AuthOverview";
-import ExampleList from "./_components/ExampleList";
-
 const Page = async () => {
   return (
-    <>
-      <AuthOverview />
-      <br />
-      <ExampleList />
-    </>
+    <div>
+      <p>Prueba</p>
+    </div>
   );
 };
 
