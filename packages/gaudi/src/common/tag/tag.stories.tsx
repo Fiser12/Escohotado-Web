a13a499e096169e78f68afb896c5744553b068a3
Mogami <mogami.creative@gmail.com>
2024-11-08 21:15:14 +0100

    tag component created

---
+++ packages/gaudi/src/common/tag/tag.stories.tsx
@@ -0,0 +1,48 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Tag } from './tag';
+
+const meta: Meta<typeof Tag> = {
+    title: 'Common/Atoms/Tag',
+    component: Tag,
+    parameters: {
+        layout: 'fullscreen',
+        design: {
+            type: 'figspec',
+            url: 'https://www.figma.com/file/xq1hrsTcwvQMgX49pwXGcc/Escohotado?node-id=55-116&m=dev'
+        }
+    },
+    argTypes: {
+        variant: {
+            control: {
+                type: 'select',
+                options: ['light', 'primary'],
+            },
+        },
+        className: {
+            control: 'text',
+        },
+    },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+//Light
+export const Light: Story = {
+    name: 'Light',
+    args: {
+        text: 'Sample tag',
+        variant: 'light',
+    },
+};
+
+//Primary
+export const Primary: Story = {
+    name: 'Primary',
+    args: {
+        text: 'Sample tag',
+        variant: 'primary',
+    },
+};
+
---
+++ packages/gaudi/src/common/tag/tag.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import classNames from 'classnames';
+
+interface Args {
+    text: string;
+    className?: string;
+    variant?: 'light' | 'primary';
+}
+
+export const Tag = ({
+    text,
+    variant = 'primary',
+    className = '',
+}: Args): JSX.Element => {
+    const tagClass = classNames(
+        'px-3 py-1 rounded-generic flex justify-center items-center text-center font-body text-xs inline-flex min-w-14 text-primary-900',
+        className,
+        {
+            'bg-white': variant === 'light',
+            'bg-primary-50': variant === 'primary',
+        }
+    );
+
+    return (
+        <div className={tagClass}>
+            {text}
+        </div>
+    );
+};
