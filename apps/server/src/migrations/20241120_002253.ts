129a2fd3d0ffe7d9ea511325f097b97e0bc5c713
Fiser <ruben@nexolabs.xyz>
2024-11-20 01:23:31 +0100

    fix: issue in the migrations

--- apps/server/src/migrations/20241120_002253.ts
+++ apps/server/src/migrations/20241120_002253.ts
@@ -44,7 +44,7 @@ export async function up({ payload, req }: MigrateUpArgs): Promise<void> {
   );
   
   CREATE TABLE IF NOT EXISTS "prices" (
-  	"id" serial PRIMARY KEY NOT NULL,
+  	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
   	"stripe_i_d" varchar NOT NULL,
   	"stripe_product_id" varchar NOT NULL,
   	"active" boolean DEFAULT false NOT NULL,
@@ -62,26 +62,27 @@ export async function up({ payload, req }: MigrateUpArgs): Promise<void> {
   
   CREATE TABLE IF NOT EXISTS "products_images" (
   	"_order" integer NOT NULL,
-  	"_parent_id" integer NOT NULL,
+  	"_parent_id" uuid NOT NULL,
   	"id" varchar PRIMARY KEY NOT NULL,
   	"url" varchar
   );
   
   CREATE TABLE IF NOT EXISTS "products_features" (
   	"_order" integer NOT NULL,
-  	"_parent_id" integer NOT NULL,
+  	"_parent_id" uuid NOT NULL,
   	"id" varchar PRIMARY KEY NOT NULL,
   	"title" varchar
   );
   
   CREATE TABLE IF NOT EXISTS "products" (
-  	"id" serial PRIMARY KEY NOT NULL,
+  	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
   	"stripe_i_d" varchar NOT NULL,
   	"type" "enum_products_type",
   	"active" boolean DEFAULT false NOT NULL,
   	"name" varchar NOT NULL,
   	"description" varchar,
   	"metadata" jsonb,
+  	"permissions_seeds" varchar DEFAULT '',
   	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
   	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL
   );
@@ -89,15 +90,16 @@ export async function up({ payload, req }: MigrateUpArgs): Promise<void> {
   CREATE TABLE IF NOT EXISTS "products_rels" (
   	"id" serial PRIMARY KEY NOT NULL,
   	"order" integer,
-  	"parent_id" integer NOT NULL,
+  	"parent_id" uuid NOT NULL,
   	"path" varchar NOT NULL,
-  	"prices_id" integer
+  	"prices_id" uuid,
+  	"permission_id" varchar
   );
   
   CREATE TABLE IF NOT EXISTS "subscriptions" (
-  	"id" serial PRIMARY KEY NOT NULL,
+  	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
   	"user_id" varchar NOT NULL,
-  	"product_id" integer NOT NULL,
+  	"product_id" uuid NOT NULL,
   	"status" "enum_subscriptions_status" NOT NULL,
   	"created" timestamp(3) with time zone,
   	"current_period_start" timestamp(3) with time zone,
@@ -112,12 +114,13 @@ export async function up({ payload, req }: MigrateUpArgs): Promise<void> {
   	"stripe_price_i_d" varchar NOT NULL,
   	"stripe_customer_id" varchar NOT NULL,
   	"metadata" jsonb,
+  	"permissions_seeds" varchar DEFAULT '',
   	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
   	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL
   );
   
   CREATE TABLE IF NOT EXISTS "media" (
-  	"id" serial PRIMARY KEY NOT NULL,
+  	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
   	"title" varchar,
   	"raw_content" varchar,
   	"prefix" varchar DEFAULT 'media',
@@ -141,24 +144,82 @@ export async function up({ payload, req }: MigrateUpArgs): Promise<void> {
   );
   
   CREATE TABLE IF NOT EXISTS "taxonomy" (
-  	"id" serial PRIMARY KEY NOT NULL,
-  	"title" varchar NOT NULL,
-  	"seed" varchar,
+  	"id" varchar PRIMARY KEY NOT NULL,
+  	"selectable" boolean DEFAULT true,
   	"slug" varchar NOT NULL,
+  	"singular_name" varchar NOT NULL,
+  	"plural_name" varchar,
+  	"seed" varchar,
+  	"parent_id" varchar,
   	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
   	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL
   );
   
-  CREATE TABLE IF NOT EXISTS "taxonomy_rels" (
+  CREATE TABLE IF NOT EXISTS "article_pdf" (
+  	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+  	"cover_id" uuid NOT NULL,
+  	"title" varchar,
+  	"description" varchar,
+  	"published_at" timestamp(3) with time zone NOT NULL,
+  	"seeds" varchar DEFAULT '',
+  	"permissions_seeds" varchar DEFAULT '',
+  	"prefix" varchar DEFAULT 'article_pdf',
+  	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"url" varchar,
+  	"thumbnail_u_r_l" varchar,
+  	"filename" varchar,
+  	"mime_type" varchar,
+  	"filesize" numeric,
+  	"width" numeric,
+  	"height" numeric,
+  	"focal_x" numeric,
+  	"focal_y" numeric
+  );
+  
+  CREATE TABLE IF NOT EXISTS "article_pdf_rels" (
   	"id" serial PRIMARY KEY NOT NULL,
   	"order" integer,
-  	"parent_id" integer NOT NULL,
+  	"parent_id" uuid NOT NULL,
   	"path" varchar NOT NULL,
-  	"taxonomy_id" integer
+  	"taxonomy_id" varchar,
+  	"permission_id" varchar
+  );
+  
+  CREATE TABLE IF NOT EXISTS "article_web" (
+  	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+  	"cover_id" uuid NOT NULL,
+  	"title" varchar,
+  	"description" varchar,
+  	"published_at" timestamp(3) with time zone NOT NULL,
+  	"seeds" varchar DEFAULT '',
+  	"permissions_seeds" varchar DEFAULT '',
+  	"slug" varchar NOT NULL,
+  	"content" jsonb,
+  	"content_html" varchar,
+  	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL
+  );
+  
+  CREATE TABLE IF NOT EXISTS "article_web_rels" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"order" integer,
+  	"parent_id" uuid NOT NULL,
+  	"path" varchar NOT NULL,
+  	"taxonomy_id" varchar,
+  	"permission_id" varchar
+  );
+  
+  CREATE TABLE IF NOT EXISTS "permission" (
+  	"id" varchar PRIMARY KEY NOT NULL,
+  	"slug" varchar NOT NULL,
+  	"title" varchar NOT NULL,
+  	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL
   );
   
   CREATE TABLE IF NOT EXISTS "payload_locked_documents" (
-  	"id" serial PRIMARY KEY NOT NULL,
+  	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
   	"global_slug" varchar,
   	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
   	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL
@@ -167,18 +228,21 @@ export async function up({ payload, req }: MigrateUpArgs): Promise<void> {
   CREATE TABLE IF NOT EXISTS "payload_locked_documents_rels" (
   	"id" serial PRIMARY KEY NOT NULL,
   	"order" integer,
-  	"parent_id" integer NOT NULL,
+  	"parent_id" uuid NOT NULL,
   	"path" varchar NOT NULL,
   	"users_id" varchar,
-  	"prices_id" integer,
-  	"products_id" integer,
-  	"subscriptions_id" integer,
-  	"media_id" integer,
-  	"taxonomy_id" integer
+  	"prices_id" uuid,
+  	"products_id" uuid,
+  	"subscriptions_id" uuid,
+  	"media_id" uuid,
+  	"taxonomy_id" varchar,
+  	"article_pdf_id" uuid,
+  	"article_web_id" uuid,
+  	"permission_id" varchar
   );
   
   CREATE TABLE IF NOT EXISTS "payload_preferences" (
-  	"id" serial PRIMARY KEY NOT NULL,
+  	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
   	"key" varchar,
   	"value" jsonb,
   	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
@@ -188,13 +252,13 @@ export async function up({ payload, req }: MigrateUpArgs): Promise<void> {
   CREATE TABLE IF NOT EXISTS "payload_preferences_rels" (
   	"id" serial PRIMARY KEY NOT NULL,
   	"order" integer,
-  	"parent_id" integer NOT NULL,
+  	"parent_id" uuid NOT NULL,
   	"path" varchar NOT NULL,
   	"users_id" varchar
   );
   
   CREATE TABLE IF NOT EXISTS "payload_migrations" (
-  	"id" serial PRIMARY KEY NOT NULL,
+  	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
   	"name" varchar,
   	"batch" numeric,
   	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
@@ -244,6 +308,12 @@ export async function up({ payload, req }: MigrateUpArgs): Promise<void> {
   END $$;
   
   DO $$ BEGIN
+   ALTER TABLE "products_rels" ADD CONSTRAINT "products_rels_permission_fk" FOREIGN KEY ("permission_id") REFERENCES "public"."permission"("id") ON DELETE cascade ON UPDATE no action;
+  EXCEPTION
+   WHEN duplicate_object THEN null;
+  END $$;
+  
+  DO $$ BEGIN
    ALTER TABLE "subscriptions" ADD CONSTRAINT "subscriptions_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE set null ON UPDATE no action;
   EXCEPTION
    WHEN duplicate_object THEN null;
@@ -256,13 +326,55 @@ export async function up({ payload, req }: MigrateUpArgs): Promise<void> {
   END $$;
   
   DO $$ BEGIN
-   ALTER TABLE "taxonomy_rels" ADD CONSTRAINT "taxonomy_rels_parent_fk" FOREIGN KEY ("parent_id") REFERENCES "public"."taxonomy"("id") ON DELETE cascade ON UPDATE no action;
+   ALTER TABLE "taxonomy" ADD CONSTRAINT "taxonomy_parent_id_taxonomy_id_fk" FOREIGN KEY ("parent_id") REFERENCES "public"."taxonomy"("id") ON DELETE set null ON UPDATE no action;
   EXCEPTION
    WHEN duplicate_object THEN null;
   END $$;
   
   DO $$ BEGIN
-   ALTER TABLE "taxonomy_rels" ADD CONSTRAINT "taxonomy_rels_taxonomy_fk" FOREIGN KEY ("taxonomy_id") REFERENCES "public"."taxonomy"("id") ON DELETE cascade ON UPDATE no action;
+   ALTER TABLE "article_pdf" ADD CONSTRAINT "article_pdf_cover_id_media_id_fk" FOREIGN KEY ("cover_id") REFERENCES "public"."media"("id") ON DELETE set null ON UPDATE no action;
+  EXCEPTION
+   WHEN duplicate_object THEN null;
+  END $$;
+  
+  DO $$ BEGIN
+   ALTER TABLE "article_pdf_rels" ADD CONSTRAINT "article_pdf_rels_parent_fk" FOREIGN KEY ("parent_id") REFERENCES "public"."article_pdf"("id") ON DELETE cascade ON UPDATE no action;
+  EXCEPTION
+   WHEN duplicate_object THEN null;
+  END $$;
+  
+  DO $$ BEGIN
+   ALTER TABLE "article_pdf_rels" ADD CONSTRAINT "article_pdf_rels_taxonomy_fk" FOREIGN KEY ("taxonomy_id") REFERENCES "public"."taxonomy"("id") ON DELETE cascade ON UPDATE no action;
+  EXCEPTION
+   WHEN duplicate_object THEN null;
+  END $$;
+  
+  DO $$ BEGIN
+   ALTER TABLE "article_pdf_rels" ADD CONSTRAINT "article_pdf_rels_permission_fk" FOREIGN KEY ("permission_id") REFERENCES "public"."permission"("id") ON DELETE cascade ON UPDATE no action;
+  EXCEPTION
+   WHEN duplicate_object THEN null;
+  END $$;
+  
+  DO $$ BEGIN
+   ALTER TABLE "article_web" ADD CONSTRAINT "article_web_cover_id_media_id_fk" FOREIGN KEY ("cover_id") REFERENCES "public"."media"("id") ON DELETE set null ON UPDATE no action;
+  EXCEPTION
+   WHEN duplicate_object THEN null;
+  END $$;
+  
+  DO $$ BEGIN
+   ALTER TABLE "article_web_rels" ADD CONSTRAINT "article_web_rels_parent_fk" FOREIGN KEY ("parent_id") REFERENCES "public"."article_web"("id") ON DELETE cascade ON UPDATE no action;
+  EXCEPTION
+   WHEN duplicate_object THEN null;
+  END $$;
+  
+  DO $$ BEGIN
+   ALTER TABLE "article_web_rels" ADD CONSTRAINT "article_web_rels_taxonomy_fk" FOREIGN KEY ("taxonomy_id") REFERENCES "public"."taxonomy"("id") ON DELETE cascade ON UPDATE no action;
+  EXCEPTION
+   WHEN duplicate_object THEN null;
+  END $$;
+  
+  DO $$ BEGIN
+   ALTER TABLE "article_web_rels" ADD CONSTRAINT "article_web_rels_permission_fk" FOREIGN KEY ("permission_id") REFERENCES "public"."permission"("id") ON DELETE cascade ON UPDATE no action;
   EXCEPTION
    WHEN duplicate_object THEN null;
   END $$;
@@ -310,6 +422,24 @@ export async function up({ payload, req }: MigrateUpArgs): Promise<void> {
   END $$;
   
   DO $$ BEGIN
+   ALTER TABLE "payload_locked_documents_rels" ADD CONSTRAINT "payload_locked_documents_rels_article_pdf_fk" FOREIGN KEY ("article_pdf_id") REFERENCES "public"."article_pdf"("id") ON DELETE cascade ON UPDATE no action;
+  EXCEPTION
+   WHEN duplicate_object THEN null;
+  END $$;
+  
+  DO $$ BEGIN
+   ALTER TABLE "payload_locked_documents_rels" ADD CONSTRAINT "payload_locked_documents_rels_article_web_fk" FOREIGN KEY ("article_web_id") REFERENCES "public"."article_web"("id") ON DELETE cascade ON UPDATE no action;
+  EXCEPTION
+   WHEN duplicate_object THEN null;
+  END $$;
+  
+  DO $$ BEGIN
+   ALTER TABLE "payload_locked_documents_rels" ADD CONSTRAINT "payload_locked_documents_rels_permission_fk" FOREIGN KEY ("permission_id") REFERENCES "public"."permission"("id") ON DELETE cascade ON UPDATE no action;
+  EXCEPTION
+   WHEN duplicate_object THEN null;
+  END $$;
+  
+  DO $$ BEGIN
    ALTER TABLE "payload_preferences_rels" ADD CONSTRAINT "payload_preferences_rels_parent_fk" FOREIGN KEY ("parent_id") REFERENCES "public"."payload_preferences"("id") ON DELETE cascade ON UPDATE no action;
   EXCEPTION
    WHEN duplicate_object THEN null;
@@ -341,6 +471,7 @@ export async function up({ payload, req }: MigrateUpArgs): Promise<void> {
   CREATE INDEX IF NOT EXISTS "products_rels_parent_idx" ON "products_rels" USING btree ("parent_id");
   CREATE INDEX IF NOT EXISTS "products_rels_path_idx" ON "products_rels" USING btree ("path");
   CREATE INDEX IF NOT EXISTS "products_rels_prices_id_idx" ON "products_rels" USING btree ("prices_id");
+  CREATE INDEX IF NOT EXISTS "products_rels_permission_id_idx" ON "products_rels" USING btree ("permission_id");
   CREATE INDEX IF NOT EXISTS "subscriptions_user_idx" ON "subscriptions" USING btree ("user_id");
   CREATE INDEX IF NOT EXISTS "subscriptions_product_idx" ON "subscriptions" USING btree ("product_id");
   CREATE INDEX IF NOT EXISTS "subscriptions_updated_at_idx" ON "subscriptions" USING btree ("updated_at");
@@ -350,12 +481,30 @@ export async function up({ payload, req }: MigrateUpArgs): Promise<void> {
   CREATE UNIQUE INDEX IF NOT EXISTS "media_filename_idx" ON "media" USING btree ("filename");
   CREATE INDEX IF NOT EXISTS "media_sizes_thumbnail_sizes_thumbnail_filename_idx" ON "media" USING btree ("sizes_thumbnail_filename");
   CREATE UNIQUE INDEX IF NOT EXISTS "taxonomy_slug_idx" ON "taxonomy" USING btree ("slug");
+  CREATE INDEX IF NOT EXISTS "taxonomy_parent_idx" ON "taxonomy" USING btree ("parent_id");
   CREATE INDEX IF NOT EXISTS "taxonomy_updated_at_idx" ON "taxonomy" USING btree ("updated_at");
   CREATE INDEX IF NOT EXISTS "taxonomy_created_at_idx" ON "taxonomy" USING btree ("created_at");
-  CREATE INDEX IF NOT EXISTS "taxonomy_rels_order_idx" ON "taxonomy_rels" USING btree ("order");
-  CREATE INDEX IF NOT EXISTS "taxonomy_rels_parent_idx" ON "taxonomy_rels" USING btree ("parent_id");
-  CREATE INDEX IF NOT EXISTS "taxonomy_rels_path_idx" ON "taxonomy_rels" USING btree ("path");
-  CREATE INDEX IF NOT EXISTS "taxonomy_rels_taxonomy_id_idx" ON "taxonomy_rels" USING btree ("taxonomy_id");
+  CREATE INDEX IF NOT EXISTS "article_pdf_cover_idx" ON "article_pdf" USING btree ("cover_id");
+  CREATE INDEX IF NOT EXISTS "article_pdf_updated_at_idx" ON "article_pdf" USING btree ("updated_at");
+  CREATE INDEX IF NOT EXISTS "article_pdf_created_at_idx" ON "article_pdf" USING btree ("created_at");
+  CREATE UNIQUE INDEX IF NOT EXISTS "article_pdf_filename_idx" ON "article_pdf" USING btree ("filename");
+  CREATE INDEX IF NOT EXISTS "article_pdf_rels_order_idx" ON "article_pdf_rels" USING btree ("order");
+  CREATE INDEX IF NOT EXISTS "article_pdf_rels_parent_idx" ON "article_pdf_rels" USING btree ("parent_id");
+  CREATE INDEX IF NOT EXISTS "article_pdf_rels_path_idx" ON "article_pdf_rels" USING btree ("path");
+  CREATE INDEX IF NOT EXISTS "article_pdf_rels_taxonomy_id_idx" ON "article_pdf_rels" USING btree ("taxonomy_id");
+  CREATE INDEX IF NOT EXISTS "article_pdf_rels_permission_id_idx" ON "article_pdf_rels" USING btree ("permission_id");
+  CREATE INDEX IF NOT EXISTS "article_web_cover_idx" ON "article_web" USING btree ("cover_id");
+  CREATE UNIQUE INDEX IF NOT EXISTS "article_web_slug_idx" ON "article_web" USING btree ("slug");
+  CREATE INDEX IF NOT EXISTS "article_web_updated_at_idx" ON "article_web" USING btree ("updated_at");
+  CREATE INDEX IF NOT EXISTS "article_web_created_at_idx" ON "article_web" USING btree ("created_at");
+  CREATE INDEX IF NOT EXISTS "article_web_rels_order_idx" ON "article_web_rels" USING btree ("order");
+  CREATE INDEX IF NOT EXISTS "article_web_rels_parent_idx" ON "article_web_rels" USING btree ("parent_id");
+  CREATE INDEX IF NOT EXISTS "article_web_rels_path_idx" ON "article_web_rels" USING btree ("path");
+  CREATE INDEX IF NOT EXISTS "article_web_rels_taxonomy_id_idx" ON "article_web_rels" USING btree ("taxonomy_id");
+  CREATE INDEX IF NOT EXISTS "article_web_rels_permission_id_idx" ON "article_web_rels" USING btree ("permission_id");
+  CREATE UNIQUE INDEX IF NOT EXISTS "permission_slug_idx" ON "permission" USING btree ("slug");
+  CREATE INDEX IF NOT EXISTS "permission_updated_at_idx" ON "permission" USING btree ("updated_at");
+  CREATE INDEX IF NOT EXISTS "permission_created_at_idx" ON "permission" USING btree ("created_at");
   CREATE INDEX IF NOT EXISTS "payload_locked_documents_global_slug_idx" ON "payload_locked_documents" USING btree ("global_slug");
   CREATE INDEX IF NOT EXISTS "payload_locked_documents_updated_at_idx" ON "payload_locked_documents" USING btree ("updated_at");
   CREATE INDEX IF NOT EXISTS "payload_locked_documents_created_at_idx" ON "payload_locked_documents" USING btree ("created_at");
@@ -368,6 +517,9 @@ export async function up({ payload, req }: MigrateUpArgs): Promise<void> {
   CREATE INDEX IF NOT EXISTS "payload_locked_documents_rels_subscriptions_id_idx" ON "payload_locked_documents_rels" USING btree ("subscriptions_id");
   CREATE INDEX IF NOT EXISTS "payload_locked_documents_rels_media_id_idx" ON "payload_locked_documents_rels" USING btree ("media_id");
   CREATE INDEX IF NOT EXISTS "payload_locked_documents_rels_taxonomy_id_idx" ON "payload_locked_documents_rels" USING btree ("taxonomy_id");
+  CREATE INDEX IF NOT EXISTS "payload_locked_documents_rels_article_pdf_id_idx" ON "payload_locked_documents_rels" USING btree ("article_pdf_id");
+  CREATE INDEX IF NOT EXISTS "payload_locked_documents_rels_article_web_id_idx" ON "payload_locked_documents_rels" USING btree ("article_web_id");
+  CREATE INDEX IF NOT EXISTS "payload_locked_documents_rels_permission_id_idx" ON "payload_locked_documents_rels" USING btree ("permission_id");
   CREATE INDEX IF NOT EXISTS "payload_preferences_key_idx" ON "payload_preferences" USING btree ("key");
   CREATE INDEX IF NOT EXISTS "payload_preferences_updated_at_idx" ON "payload_preferences" USING btree ("updated_at");
   CREATE INDEX IF NOT EXISTS "payload_preferences_created_at_idx" ON "payload_preferences" USING btree ("created_at");
@@ -381,24 +533,28 @@ export async function up({ payload, req }: MigrateUpArgs): Promise<void> {
 
 export async function down({ payload, req }: MigrateDownArgs): Promise<void> {
   await payload.db.drizzle.execute(sql`
-   DROP TABLE "users_accounts";
-  DROP TABLE "users_sessions";
-  DROP TABLE "users_verification_tokens";
-  DROP TABLE "users";
-  DROP TABLE "prices";
-  DROP TABLE "products_images";
-  DROP TABLE "products_features";
-  DROP TABLE "products";
-  DROP TABLE "products_rels";
-  DROP TABLE "subscriptions";
-  DROP TABLE "media";
-  DROP TABLE "taxonomy";
-  DROP TABLE "taxonomy_rels";
-  DROP TABLE "payload_locked_documents";
-  DROP TABLE "payload_locked_documents_rels";
-  DROP TABLE "payload_preferences";
-  DROP TABLE "payload_preferences_rels";
-  DROP TABLE "payload_migrations";
+   DROP TABLE "users_accounts" CASCADE;
+  DROP TABLE "users_sessions" CASCADE;
+  DROP TABLE "users_verification_tokens" CASCADE;
+  DROP TABLE "users" CASCADE;
+  DROP TABLE "prices" CASCADE;
+  DROP TABLE "products_images" CASCADE;
+  DROP TABLE "products_features" CASCADE;
+  DROP TABLE "products" CASCADE;
+  DROP TABLE "products_rels" CASCADE;
+  DROP TABLE "subscriptions" CASCADE;
+  DROP TABLE "media" CASCADE;
+  DROP TABLE "taxonomy" CASCADE;
+  DROP TABLE "article_pdf" CASCADE;
+  DROP TABLE "article_pdf_rels" CASCADE;
+  DROP TABLE "article_web" CASCADE;
+  DROP TABLE "article_web_rels" CASCADE;
+  DROP TABLE "permission" CASCADE;
+  DROP TABLE "payload_locked_documents" CASCADE;
+  DROP TABLE "payload_locked_documents_rels" CASCADE;
+  DROP TABLE "payload_preferences" CASCADE;
+  DROP TABLE "payload_preferences_rels" CASCADE;
+  DROP TABLE "payload_migrations" CASCADE;
   DROP TYPE "public"."enum_prices_type";
   DROP TYPE "public"."enum_prices_interval";
   DROP TYPE "public"."enum_products_type";
--- apps/server/src/migrations/index.ts
+++ apps/server/src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20241108_160618_init from './20241108_160618_init';
+import * as migration_20241120_002253 from './20241120_002253';
 
 export const migrations = [
   {
-    up: migration_20241108_160618_init.up,
-    down: migration_20241108_160618_init.down,
-    name: '20241108_160618_init'
+    up: migration_20241120_002253.up,
+    down: migration_20241120_002253.down,
+    name: '20241120_002253'
   },
 ];
