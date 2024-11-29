7da68567ad7edd748d8bea7b12e83443e4497a18
Fiser <ruben@nexolabs.xyz>
2024-11-29 22:26:04 +0100

    fix: suicidal issue

--- apps/server/src/app/(web)/layout.tsx
+++ apps/server/src/app/(web)/layout.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import "../../tailwind.css";
 import { Header } from "gaudi/server";
 import { signIn, signOut } from "@/plugins/authjs/plugin";
 import { accountMenuBuilder } from "@/domain/accountMenuBuilder";
 import { getPayloadUser } from "@/plugins/authjs/getPayloadUser";
 import { DataFromCollectionSlug } from "payload";
 import { NuqsAdapter } from 'nuqs/adapters/next'
+import "./tailwind.css";
 
 const Layout: React.FC<{ children: React.ReactNode }> = async ({ children }) => {
   const payloadUser = await getPayloadUser<DataFromCollectionSlug<"users">>();
