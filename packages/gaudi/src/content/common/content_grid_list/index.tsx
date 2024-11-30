f8afa6b6154377d056c7f0397336649ca6f6eea2
Fiser <ruben@nexolabs.xyz>
2024-12-01 00:09:28 +0100

    fix: improved the grid

--- packages/gaudi/src/content/common/content_grid_list/index.tsx
+++ packages/gaudi/src/content/common/content_grid_list/index.tsx
@@ -2,17 +2,23 @@ interface Props<T> {
     items: T[];
     renderBox: (item: T, index: number) => JSX.Element;
     gridCols?: string;
+    gridColsSm?: string;
+    gridColsMd?: string;
+    gridColsLg?: string;
     gap?: string;
 }
 
 export const ContentGridList = <T,>({ 
     items, 
     renderBox, 
-    gridCols = "md:grid-cols-3",
+    gridCols = "grid-cols-1",
+    gridColsSm = "sm:grid-cols-2",
+    gridColsMd = "md:grid-cols-3",
+    gridColsLg = "lg:grid-cols-4",
     gap = "gap-5"
 }: Props<T>): JSX.Element => {
     return (
-        <div className={`grid grid-cols-1 ${gridCols} ${gap}`}>
+        <div className={`grid ${gridCols} ${gridColsSm} ${gridColsMd} ${gridColsLg} ${gap}`}>
             {items.map((item, index) => (
                 <div key={index}>
                     {renderBox(item, index)}
