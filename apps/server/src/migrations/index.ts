6aca9c9b01c1bd34ef816d9cab13dec1dd34ea1e
Fiser <ruben@nexolabs.xyz>
2024-12-23 03:02:23 +0100

    feat: search_results_rels added

--- apps/server/src/migrations/index.ts
+++ apps/server/src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20241204_013333 from './20241204_013333';
 import * as migration_20241213_214436 from './20241213_214436';
+import * as migration_20241223_015558 from './20241223_015558';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20241213_214436.up,
     down: migration_20241213_214436.down,
-    name: '20241213_214436'
+    name: '20241213_214436',
+  },
+  {
+    up: migration_20241223_015558.up,
+    down: migration_20241223_015558.down,
+    name: '20241223_015558'
   },
 ];
