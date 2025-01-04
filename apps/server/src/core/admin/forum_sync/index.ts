a5b4c55e0ffc55269d5cbb6cd4b9cac42437f182
Fiser <ruben@nexolabs.xyz>
2025-01-04 20:35:39 +0100

    fix: issue loading data parallel to secuential to the forum

--- apps/server/src/core/admin/forum_sync/index.ts
+++ apps/server/src/core/admin/forum_sync/index.ts
@@ -3,16 +3,18 @@ import { getPayload } from '../../infrastructure/payload/utils/getPayload'
 import { updateForumDataAtCollection } from './update_forum_data_at_collection'
 import { getCategoryByCollection } from './get_category_by_collection'
 import 'hegel'
-
 const syncForumWithDatabase = async (collection: CollectionSlug) => {
-  const payload = await getPayload()
-  payload.logger.warn(`Starting sync of forum content to ${collection} collection`)
+  const payload = await getPayload();
+  payload.logger.warn(`Starting sync of forum content to ${collection} collection`);
+
   const datas = await payload.find({
     collection,
     pagination: false,
     select: { id: true, forum_post_id: true, title: true, slug: true },
-  })
-  await datas.docs.map(async ({ forum_post_id, id, title }: any) => {
+  });
+
+  for (const doc of datas.docs) {
+    const { forum_post_id, id, title, slug } = doc as any;
     try {
       await updateForumDataAtCollection(
         payload,
@@ -20,14 +22,15 @@ const syncForumWithDatabase = async (collection: CollectionSlug) => {
         id,
         forum_post_id,
         title,
-        getCategoryByCollection(collection, id),
-      )
+        getCategoryByCollection(collection, slug),
+      );
+      payload.logger.info(`Actualizado correctamente: ${id}`);
     } catch (error) {
-      payload.logger.error(`Error updating ${id}: ${error}`)
+      payload.logger.error(`Error actualizando ${id}: ${error}`);
     }
-  })
+  }
 
-  payload.logger.warn('Sincronización completada.')
-}
+  payload.logger.warn('Sincronización completada.');
+};
 
 export default syncForumWithDatabase
