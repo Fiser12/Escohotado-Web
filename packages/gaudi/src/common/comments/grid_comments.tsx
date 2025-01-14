3dd6b261b28167f155d12ec252928107c3999b92
Mogami <mogami.creative@gmail.com>
2025-01-14 20:11:11 +0100

    Comment section Button to Foro added

--- packages/gaudi/src/common/comments/grid_comments.tsx
+++ packages/gaudi/src/common/comments/grid_comments.tsx
@@ -1,4 +1,6 @@
 import { H4 } from "../headers/H4";
+import { PenIcon } from "../icons/pen_icon";
+import { MainButton } from "../main_button/main_button";
 
 interface Props<T> {
       items: T[];
@@ -11,8 +13,14 @@ export const GridComments = <T,>({
 }: Props<T>): JSX.Element => {
       return (
             <div className="w-full pb-8">
-                  <div className="w-full pb-4 border-b border-gray-400 mb-7">
+                  <div className="w-full flex justify-between items-center pb-4 border-b border-gray-400 mb-7">
                   <H4 label="Comentarios"/>
+                  <MainButton 
+                        text="Únete al debate" 
+                        color="secondary"
+                        type="fill"
+                        icon={<PenIcon />}
+                  />
                   </div>
                   <div className="flex flex-col gap-7">
                         {items.map((item, index) => <div key={index}>
