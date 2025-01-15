976f994ad1b3157e706bffa36465ce8586c75a14
Mogami <mogami.creative@gmail.com>
2025-01-15 18:39:19 +0100

    featured article fixed grid, no useRef useState or UseEffect

--- packages/gaudi/src/content/featured_grid_home/article/index.tsx
+++ packages/gaudi/src/content/featured_grid_home/article/index.tsx
@@ -1,4 +1,3 @@
-import { useRef, useState, useEffect } from "react";
 import classNames from "classnames";
 import { Tag } from "../../../common/tag/tag";
 import { H3 } from "../../../common/headers/H3";
@@ -19,35 +18,10 @@ interface Props {
 }
 
 export const FeaturedArticle = (props: Props) => {
-  const containerRef = useRef<HTMLDivElement | null>(null);
-  const [isVertical, setIsVertical] = useState(false);
-
-  useEffect(() => {
-    const container = containerRef.current;
-
-    if (!container) return;
-
-    const resizeObserver = new ResizeObserver((entries) => {
-      for (let entry of entries) {
-        const width = entry.contentRect.width;
-        setIsVertical(width <= 300);
-      }
-    });
-
-    resizeObserver.observe(container);
-
-    return () => {
-      resizeObserver.disconnect();
-    };
-  }, []);
 
   const containerClass = classNames(
     props.className,
-    "w-full h-full md:min-h-[360px] max-h-[500px] grid gap-2 bg-white p-1 rounded",
-    {
-      "grid-cols-1": isVertical || window.innerWidth < 640,
-      "grid-cols-2": !isVertical && window.innerWidth >= 640,
-    }
+    "w-full h-full md:min-h-[360px] max-h-[500px] grid gap-2 bg-white p-1 rounded @sm:grid-cols-1 @lg:grid-cols-2",
   );
 
   const imageClass = classNames(
@@ -63,26 +37,28 @@ export const FeaturedArticle = (props: Props) => {
   const authorClass = classNames("text-xs text-gray-dark");
 
   return (
-    <div className={containerClass} ref={containerRef}>
-      <div className="relative w-full rounded overflow-hidden hidden sm:block">
-        <Image
-          fill
-          src={props.coverHref}
-          alt={props.title}
-          className={imageClass}
-        />
-      </div>
-      <div className={contentClass}>
-        <div className={textareaClass}>
-          <div className={categoriesClass}>
-            {props.categories?.map((category, index) => (
-              <Tag key={index} text={category.singular_name}></Tag>
-            ))}
-          </div>
-          <H3 label={props.title} className="line-clamp-3" />
-          <p className={authorClass}>{props.author}</p>
+    <div className="@container">
+      <div className={containerClass}>
+        <div className="relative w-full rounded overflow-hidden hidden sm:block">
+          <Image
+            fill
+            src={props.coverHref}
+            alt={props.title}
+            className={imageClass}
+          />
         </div>
-        <Link text="Leer más" href={props.href} />
+        <div className={contentClass}>
+          <div className={textareaClass}>
+            <div className={categoriesClass}>
+              {props.categories?.map((category, index) => (
+                <Tag key={index} text={category.singular_name}></Tag>
+              ))}
+            </div>
+            <H3 label={props.title} className="line-clamp-3" />
+            <p className={authorClass}>{props.author}</p>
+          </div>
+          <Link text="Leer más" href={props.href} />
+        </div>
       </div>
     </div>
   );
