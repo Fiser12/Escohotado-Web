9576b414ffa85162f1678854835077a65d8e1c22
Fiser <ruben@nexolabs.xyz>
2025-02-15 13:14:29 +0100

    feat: mdx enabled

--- apps/server/.storybook/main.ts
+++ apps/server/.storybook/main.ts
@@ -10,7 +10,10 @@ function getAbsolutePath(value: string): any {
   return dirname(require.resolve(join(value, 'package.json')))
 }
 const config: StorybookConfig = {
-  stories: ['../../../packages/gaudi/src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: [
+    '../../../packages/gaudi/src/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+    '../../../packages/gaudi/src/**/*.mdx'
+  ],
   staticDirs: ['../public'],
   addons: [
     getAbsolutePath('@storybook/addon-links'),
