54f87c0ae7eff163ab0ba56c746480c79553a863
Fiser <ruben@nexolabs.xyz>
2025-04-03 19:24:25 +0200

    chore: trigger storybook build

--- apps/server/src/components/assets/index.ts
+++ apps/server/src/components/assets/index.ts
@@ -1,3 +1,4 @@
 import handwrittenBackground from "./images/handwritting-bg.jpg";
 import escohotadoArticlesPortada from "./images/antonio-escohotado-lecturas.png";
 export { handwrittenBackground, escohotadoArticlesPortada };
+
